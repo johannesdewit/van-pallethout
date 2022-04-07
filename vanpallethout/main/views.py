f2470b3bdd5f17103e09ad31f92fe85cc532d637
Johannes de Wit <77152231+johannesdewit@users.noreply.github.com>
2022-04-07 11:12:58 +0200

    Urls setup

--- vanpallethout/main/views.py
+++ vanpallethout/main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, you're at the index!")
+
+def contact(request):
+    return HttpResponse("Hello, you're at the contact page!")
+
+def workshops(request):
+    return HttpResponse("Hello, you're at the workshops page!")
